cc28c803397560c6c1c8f1f29f89b1463336fcb8
Areefa Samar <samar4802098@cloud.neduet.edu.pk>
2025-10-27 16:48:50 +0500

    Create Two Sum.c

---
+++ Two Sum.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    *returnSize = 2; // because always two indices returned
+    int* result = (int*)malloc(2 * sizeof(int));
+    
+    for (int i = 0; i < numsSize; i++) {
+        for (int j = i + 1; j < numsSize; j++) {
+            if (nums[i] + nums[j] == target) {
+                result[0] = i;
+                result[1] = j;
+                return result;
+            }
+        }
+    }
+    
+    // If no solution found (should not happen as per problem statement)
+    *returnSize = 0;
+    free(result);
+    return NULL;
+}
