74890af59c6c8bd2b5be83e9c7b55535b1af4e09
Areefa Samar <samar4802098@cloud.neduet.edu.pk>
2025-10-27 18:12:19 +0500

    Update Number of steps to reduce a Number to Zero.c

--- Number of steps to reduce a Number to Zero.c
+++ Number of steps to reduce a Number to Zero.c
@@ -1,28 +1,24 @@
 #include <stdio.h>
 
-// Function to return number of steps to reduce num to zero
-int numberOfSteps(int num) {
+int numberOfSteps(int num){
     int steps = 0;
-
-    while (num > 0) {
-        if (num % 2 == 0) {
-            num = num / 2;   // if even, divide by 2
-        } else {
-            num = num - 1;   // if odd, subtract 1
-        }
-        steps++;              // count each step
-    }
-
-    return steps;
-}
-
-int main() {
-    int n;
-    printf("Enter a number: ");
-    scanf("%d", &n);
-
-    int result = numberOfSteps(n);
-    printf("Number of steps to reduce %d to zero = %d\n", n, result);
-
-    return 0;
+    while (num > 0){
+          if (num % 2 == 0){
+                  num = num/2;
+          }
+          else{
+               num = num - 1;
+          }        
+          steps ++;
+          }
+          return steps;
+     }
+     int main(){
+         int n;
+         printf("Enter a number: ");
+         scanf("%d",&n);
+         
+     int result = numberOfSteps(n);
+     printf("Number of steps to reduce %d to zero = %d\n",n, result);
+      return 0;
 }
