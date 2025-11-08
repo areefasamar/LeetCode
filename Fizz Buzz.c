05cb8e52413be89da7c15915c4a1b55c6e69ca3b
Areefa Samar <samar4802098@cloud.neduet.edu.pk>
2025-11-08 06:52:37 +0500

    Implement Fizz Buzz program in C

---
+++ Fizz Buzz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    int n;
+    printf("Enter value of n: ");
+    scanf("%d", &n);
+
+    for(int i = 1; i <= n; i++) {
+        if(i % 3 == 0 && i % 5 == 0)
+            printf("FizzBuzz\n");
+        else if(i % 3 == 0)
+            printf("Fizz\n");
+        else if(i % 5 == 0)
+            printf("Buzz\n");
+        else
+            printf("%d\n", i);
+    }
+
+    return 0;
+}
