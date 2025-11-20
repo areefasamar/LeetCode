e79fcbebab7d276ce16fdffa622c443e55ee199a
Areefa Samar <samar4802098@cloud.neduet.edu.pk>
2025-11-20 08:30:17 +0500

    Create Remove Element.c

---
+++ Remove Element.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int removeElement(int* nums, int numsSize, int val) {
+    int k = 0;
+
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] != val) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+
+    return k;
+}
+
+int main() {
+    int nums[] = {3, 2, 2, 3, 4, 3};
+    int numsSize = sizeof(nums) / sizeof(nums[0]);
+    int val = 3;
+
+    int k = removeElement(nums, numsSize, val);
+
+    printf("k = %d\n", k);
+    printf("Updated array: ");
+    for (int i = 0; i < k; i++) {
+        printf("%d ", nums[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
